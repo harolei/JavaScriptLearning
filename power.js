6b4e8e790604cbd187ad6955ed93db0a5bec4ffb
harolei <harolei@gmail.com>
2013-05-03 09:53:10 +0800

    move the codes to the folder.

--- power.js
+++
@@ -1,9 +0,0 @@
-function power(base, exponent){
-	var result = 1;
-	for(var count = 0; count < exponent; count++){
-	result *= base;
-	}
-	return result;
-}
-
-alert(power(2,10));
--- scope.js
+++
@@ -1,6 +0,0 @@
-var something = 1;
-{
-	var something = 2;
-	document.write("Inside: " + something + '\n');
-}
-document.write("Outside: " + something);	
